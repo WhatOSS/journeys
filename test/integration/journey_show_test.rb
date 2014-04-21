f2781ccbbaa65b5ed720202001d1f3743ce72980
th3james <th3james@fastmail.fm>
2014-04-21 15:50:36 +0100

    Add first acceptance test for journey show page

---
+++ test/integration/journey_show_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+require 'nokogiri'
+
+class JourneyShowTest < ActionDispatch::IntegrationTest
+  test "visiting a journey shows the list of slugs for the associated events" do
+    journey = Journey.create()
+    Event.create({
+      journey: journey,
+      slug: "/"
+    })
+    Event.create({
+      journey: journey,
+      slug: "/search?q=\"peaches\""
+    })
+
+    get "/journeys/#{journey.id}"
+
+    assert_response :success
+
+    assert_select ".events", {
+      count: 1
+    }
+
+    page = Nokogiri::HTML(response.body)
+
+    events = page.css('.events > li')
+
+    assert_equal events.length, 2,
+      "Expected to see both events"
+
+    assert_equal events[0].css('.slug')[0].content, '/',
+      "Expected to see the slug for the first event"
+
+    assert_equal events[1].css('.slug')[0].content, '/search?q="peaches"',
+      "Expected to see the slug for the first event"
+  end
+end
