fc7e96184d56da8ec396156d053959ab07824f26
Adam Mulligan <adammulligan@gmail.com>
2014-04-23 13:47:23 +0100

    Require a separate secrets yaml file

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'https://github.com/WhatOSS/journeys.git'
 
 set :branch, "deployment"
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
