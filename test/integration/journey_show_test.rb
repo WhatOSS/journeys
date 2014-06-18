16df433977d884ed6e281af63777fa2f48d53083
th3james <th3james@fastmail.fm>
2014-06-18 13:52:31 +0100

    Show time stamps for events on journey show

--- test/integration/journey_show_test.rb
+++ test/integration/journey_show_test.rb
@@ -34,4 +34,34 @@ class JourneyShowTest < ActionDispatch::IntegrationTest
     assert_equal '/search?q="peaches"', events[1].css('.slug')[0].content,
       "Expected to see the slug for the first event"
   end
+
+  test "visiting a journey shows the time next to the first event,
+  and the time that passes to the second event" do
+    event_start = 5.minutes.ago
+    journey = Journey.create!(user: User.create())
+    Event.create!(journey: journey, created_at: event_start)
+    second_event_time = event_start + 2.minutes
+    Event.create!(journey: journey, created_at: second_event_time)
+
+    get "/journeys/#{journey.id}"
+
+    assert_response :success
+
+    page = Nokogiri::HTML(response.body)
+
+    events = page.css('.events > li')
+
+    assert_equal(
+      event_start.strftime("%H:%M:%S"),
+      events.first.css('.time').first.content.strip,
+      "Expected to see the correct time on the first event"
+    )
+
+    assert_equal(
+      second_event_time.strftime("%H:%M:%S"),
+      events.last.css('.time').first.content.strip,
+      "Expected to see the correct time on the second event"
+    )
+  end
+
 end
